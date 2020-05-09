fd677bc937196017e91510ff51ec432066d11e9a
reginaldo <camilo.melges@gmail.com>
2020-05-09 04:10:43 -0400

    added config of database to application-dev propertiers and annotaions for default config in DemoApplication

--- backend/src/main/java/com/desafio/desafio/DemoApplication.java
+++ backend/src/main/java/com/desafio/desafio/DemoApplication.java
@@ -1,9 +1,19 @@
 package com.desafio.desafio;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement
+@EnableCaching
+@EnableSpringDataWebSupport
+@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
