84c970b39e6bd799bcf2a4ce3ec12892167c44e9
reginaldo <camilo.melges@gmail.com>
2020-05-10 03:01:48 -0400

    finally finished config database with docker-compose

--- backend/src/main/resources/db/changelog/db.mysql.sql
+++ backend/src/main/resources/db/changelog/db.mysql.sql
@@ -1,4 +1,10 @@
 --liquibase formatted sql
 
---changeset reginaldo:1587663728087-1
-CREATE TABLE biddings (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, description TEXT, type ENUM("Menor Preço", "Nota Preço") default "Menor Preço");
+--changeset reginaldo:1589088050671-1
+CREATE TABLE biddings (id INT AUTO_INCREMENT NOT NULL, `description` TEXT NULL, type ENUM("Nota Preço", "Menor Preço") DEFAULT "Menor Preço" NULL, CONSTRAINT PK_BIDDINGS PRIMARY KEY (id));
+
+--changeset reginaldo:1589088050671-2
+CREATE TABLE proposal (id INT AUTO_INCREMENT NOT NULL, `provider` VARCHAR(50) NULL, note DECIMAL(19,2) NULL DEFAULT 0.00, price DECIMAL(19,2) NULL DEFAULT 0.00, created_date VARCHAR(10) NULL, classification INT(11) NULL, biddingId INT(11) NOT NULL, CONSTRAINT PK_PROPOSAL PRIMARY KEY (id));
+
+--changeset reginaldo:1589088050671-3
+ALTER TABLE proposal ADD CONSTRAINT PK_PROPOSAL_BIDDING_ID FOREIGN KEY (biddingId) REFERENCES biddings (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
