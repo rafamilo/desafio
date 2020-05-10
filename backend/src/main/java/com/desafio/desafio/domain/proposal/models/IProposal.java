12060dbbdee30049e32401cfd9b43b38dc638d9f
reginaldo <camilo.melges@gmail.com>
2020-05-10 18:13:24 -0400

    Added data of proposal in model

--- backend/src/main/java/com/desafio/desafio/domain/proposal/models/IProposal.java
+++ backend/src/main/java/com/desafio/desafio/domain/proposal/models/IProposal.java
@@ -1,5 +1,20 @@
 package com.desafio.desafio.domain.proposal.models;
 
-public interface IProposal {
+import java.math.BigDecimal;
 
+import com.desafio.desafio.domain.bidding.models.IBidding;
+
+public interface IProposal {
+  public Integer getId();
+  public void setId(Integer id);
+  public String getProvider();
+  public void setProvider(String provider);
+  public BigDecimal getNote();
+  public void setNote(BigDecimal note);
+  public BigDecimal getPrice();
+  public void setPrice(BigDecimal price);
+  public String getCreatedDate();
+  public void setCreatedDate(String createdDate);
+  public IBidding getBidding();
+  public void setBidding(IBidding bidding);
 }
--- backend/src/main/java/com/desafio/desafio/domain/proposal/models/Proposal.java
+++ backend/src/main/java/com/desafio/desafio/domain/proposal/models/Proposal.java
@@ -1,10 +1,15 @@
 package com.desafio.desafio.domain.proposal.models;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.desafio.desafio.domain.bidding.models.IBidding;
 
 import org.springframework.stereotype.Service;
 
@@ -16,4 +21,67 @@ public class Proposal implements IProposal {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+
+  @Column
+  private String provider;
+
+  @Column
+  private BigDecimal note;
+
+  @Column
+  private BigDecimal price;
+
+  @Column
+  private String createdDate;
+
+  @Column
+  private IBidding bidding;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getProvider() {
+    return provider;
+  }
+
+  public void setProvider(String provider) {
+    this.provider = provider;
+  }
+
+  public BigDecimal getNote() {
+    return note;
+  }
+
+  public void setNote(BigDecimal note) {
+    this.note = note;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public String getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(String createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public IBidding getBidding() {
+    return bidding;
+  }
+
+  public void setBidding(IBidding bidding) {
+    this.bidding = bidding;
+  }
 }
