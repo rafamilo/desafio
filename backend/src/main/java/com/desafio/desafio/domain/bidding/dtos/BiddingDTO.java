8a5f4764c30b4b178f6d9a654c5dfc62582dda56
reginaldo <camilo.melges@gmail.com>
2020-05-09 21:41:09 -0400

    Create BiddingDTO

---
+++ backend/src/main/java/com/desafio/desafio/domain/bidding/dtos/BiddingDTO.java
@@ -0,0 +1,37 @@
+package com.desafio.desafio.domain.bidding.dtos;
+
+public class BiddingDTO implements IBiddingDTO {
+  private Integer id;
+  private String description;
+  private String type;
+
+  @Override
+  public Integer getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
+  }
+
+  @Override
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String getType() {
+    return type;
+  }
+
+  @Override
+  public void setType(String type) {
+    this.type = type;
+  }
+}
---
+++ backend/src/main/java/com/desafio/desafio/domain/bidding/dtos/IBiddingDTO.java
@@ -0,0 +1,10 @@
+package com.desafio.desafio.domain.bidding.dtos;
+
+public interface IBiddingDTO {
+  public Integer getId();
+  public void setId(Integer id);
+  public String getDescription();
+  public void setDescription(String description);
+  public String getType();
+  public void setType(String type);
+}
